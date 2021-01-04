4595fbb2be29724c709420a261108b0e1cb7cdf5
Mohamed Mahrous <m.mahrous.94@gmail.com>
2021-01-04 02:58:35 +0200

    feat(Images): Allow server to handle binary data

--- src/http_server.js
+++ src/http_server.js
@@ -1,34 +1,61 @@
 const http = require('http');
 const TcpServer = require('./tcp_server');
 const zlib = require('zlib');
+const { nanoid } = require('nanoid');
 
 class Server {
 	constructor(port, tcpPort) {
 		this._port = port;
 		this._http = http.createServer(this._onRequest.bind(this))
 		this._tcpServer = new TcpServer(tcpPort);
+		this._responses = {}
 	}
 
 	_onRequest(req, res) {
 		const socketId = req.headers.host.split('.')[0];
 		const socket = this._tcpServer.getSocket(socketId);
 		socket.resume()
+		let id = nanoid();
+		this._responses[socketId] = res;
 		socket.write(JSON.stringify({
 			url: req.url,
 			method: req.method,
-			headers: req.headers
+			headers: req.headers,
+			id,
 		}))
+		
 		const dataListener = (chunk) => {
-			const resData = JSON.parse(zlib.inflateSync(Buffer.from(chunk.toString(), 'base64')).toString())
-			res.writeHead(resData.statusCode, resData.headers)
-			res.end(resData.body, () => {
-				socket.removeListener('data', dataListener)
-				socket.pause()
-			});
+			let _id
+			if (chunk.toString().startsWith('CH')) {
+				_id = chunk.toString().slice(2)
+			} else if (chunk.toString() === 'EOR') {
+				res.end(() => {
+					socket.removeListener('data', dataListener)
+					socket.pause()
+				});
+			} else {
+				res.write(chunk)
+			}
+			// 	// let data = chunk.toString().slice(0, -3)
+			// 	// console.log(data);
+			// 	// if (data !== '') res.write(Buffer.from(data))
+			// 	// res.end(() => {
+			// 	// 	socket.removeListener('data', dataListener)
+			// 	// 	socket.pause()
+			// 	// });
+			// 	this._sockets[socketId][id].end()
 
+			// } else {
+			// 	this._sockets[socketId][id].write(chunk)
+			// }
 		}
 
 		socket.on('data', dataListener);
+	}
+
+	_inflate(data) {
+		return data
+		// return zlib.inflateSync(Buffer.from(data, 'base64')).toString()
 	}
 
 	listen(callback) {
